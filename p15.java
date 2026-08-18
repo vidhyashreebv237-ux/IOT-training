08504793276677057bd618a266920999736871f2
vidhyashreebv237-ux <vidhyashreebv237@gmail.com>
2026-08-18 12:46:51 +0530

    Add files via upload

---
+++ p15.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class p15 {
+public static voud main(String[] args) {
+System.out.println("1. Right Triangle");
+
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
+}
+}
+
---
+++ p16.java
@@ -0,0 +1,42 @@
+package day2;
+
+	import java.util.Scanner;
+
+	public class p16 {
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int num = sc.nextInt();
+
+	        int original = num;
+	        int sum = 0;
+	        int digits = 0;
+
+	        // Count the number of digits
+	        int temp = num;
+
+	        while (temp != 0) {
+	            digits++;
+	            temp = temp / 10;
+	        }
+
+	        // Find the sum of powers of digits
+	        temp = num;
+
+	        while (temp != 0) {
+	            int digit = temp % 10;
+	            sum += Math.pow(digit, digits);
+	            temp = temp / 10;
+	        }
+
+	        // Check Armstrong
+	        if (sum == original) {
+	            System.out.println(original + " is an Armstrong number");
+	        } else {
+	            System.out.println(original + " is not an Armstrong number");
+	        }
+	    }
+	}
+

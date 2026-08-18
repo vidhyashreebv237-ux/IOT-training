998f2cf383402b44c56de7c8484415a65aae6753
vidhyashreebv237-ux <vidhyashreebv237@gmail.com>
2026-08-18 16:08:51 +0530

    Add files via upload

---
+++ p22.java
@@ -0,0 +1,20 @@
+package day2;
+import java.util.Scanner;
+
+public class p22 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reverse);
+    }
+}

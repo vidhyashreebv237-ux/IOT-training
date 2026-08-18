85804a78d06bd8c853c269333cac61ca1e0a97c3
vidhyashreebv237-ux <vidhyashreebv237@gmail.com>
2026-08-18 12:12:10 +0530

    Add files via upload

---
+++ p10.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class p10 {
+	public p10() {
+		System.out.println("Hello world");
+	}
+	public p10(int a) {
+		System.out.println("Hello girl");
+	}
+	public static void main(String[]args) {
+		p10 gg=new p10();
+		p10 gf=new p10(4);
+	}
+}

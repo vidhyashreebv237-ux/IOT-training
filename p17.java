7086954603c340b760ddc9f15a3d3611335f82fd
vidhyashreebv237-ux <vidhyashreebv237@gmail.com>
2026-08-18 13:09:23 +0530

    Add files via upload

---
+++ p17.java
@@ -0,0 +1,23 @@
+package day2;
+class parent
+{
+   int a =10;
+		   int b =20;
+}
+
+public class p17 extends parent {
+	int a=20;
+	int b =30;
+	
+	void add(int a , int b)
+	{
+		System.out.println(super.a+super.b);
+		System.out.println(this.a+this.b);
+		System.out.println(a+b);
+	}
+public static void main(String[] args) {
+	p17 jj = new p17();
+	jj.add(2, 3);
+	
+}
+}
